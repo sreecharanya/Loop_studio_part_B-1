e36543070470f739c8f4f968b3851ed0b8124269
Sreecharanya Srinivasan <sree3charanya@gmail.com>
2021-03-09 05:30:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//Construct a for loop that combines two strings together, alternating the characters from each source.  Print the result.
+let  str= 'Rutabagas';
+let  str1= 'Chemistry';
+// let arr=str.split('')
+// let arr1=str1.split('')
+let output;
+// console.log(arr)
+// console.log(arr1)
+//Example: If string = “1234” and otherString = “5678”, then the output will be “15263748”.
+for(let i=0;i<str.length;i++){
+output=String(output)+str[i]+str1[i];
+
+}
+console.log(output)
+//For now, make sure the two strings contain the same number of characters!
+
