8fb5b9246baaa9429eedb881444f05a401341654
Sreecharanya Srinivasan <sree3charanya@gmail.com>
2021-03-09 05:35:38 +0000

    done

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ let  str= 'Rutabagas';
 let  str1= 'Chemistry';
 // let arr=str.split('')
 // let arr1=str1.split('')
-let output;
+let output='';
+console.log(typeof output)
 // console.log(arr)
 // console.log(arr1)
 //Example: If string = “1234” and otherString = “5678”, then the output will be “15263748”.
 for(let i=0;i<str.length;i++){
-output=String(output)+str[i]+str1[i];
+output=output+str[i]+str1[i];
 
 }
 console.log(output)
+console.log(output.trim().length)
 //For now, make sure the two strings contain the same number of characters!
 
